1f2462e6f623c9bf0e8ab70358dba2b0168a4f44
cbitting1 <54864328+cbitting1@users.noreply.github.com>
2020-01-10 14:12:37 -0700

    Complete Discussion on Properties for Classes

--- OOPsReview/ConsoleApp/Window.cs
+++ OOPsReview/ConsoleApp/Window.cs
@@ -22,13 +22,15 @@ namespace ConsoleApp
 
         //Private data member
         private string _Manufacturer;
-
+        private decimal _Height;
         //Properties 
         //Properties can be implemented in two ways
             //a) Fully Implemented property
                 //used because there is additional code/logic used in processing the data
             //b) Auto Implemented Property
                 //used when there is no need for additional code/logic, when the data is simply saved
+
+            //example of a Fully implemented property
         public string Manufacturer
         {
             //der code wird an _Manufacturer geschickt
@@ -49,8 +51,64 @@ namespace ConsoleApp
             set
             {
                 //the set takes incoming data and places that data into the private data member
+
+                //internal to the property, incoming data willnbe placed in a common variable called value
+                //a property is associated with a signle data member.
+                //a property has no parameter list
+                if (string.IsNullOrEmpty(value))
+                {
+                    _Manufacturer = null;
+                }
+                else
+                {
+                    _Manufacturer = value;
+                }
+
+                //alternate code
+                _Manufacturer = string.IsNullOrEmpty(value) ? null : value;
             }
 
         }
+
+
+
+        //example of an Auto Impemented Property
+        //Auto Implemented properties can be used when there is NO NEED for additional processing against the incoming data
+        //NO internal private data member is required for this property
+        //the system will internally generate a data area for the data 
+        //access to the stored data (getting or setting) CAN ONLY be done via the property
+        public decimal Width { get; set; }
+
+        //lets fully implement Height with a criteria of being > 0
+        public decimal lHeight
+        {
+            get
+            {
+                return _Height;
+            }
+            set
+            {
+                //the m indicates the value is a decimal
+                if (value <= 0.0m)
+                {
+                    throw new Exception("Height can not be 0 or less than 0.")
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        public decimal Height { get; set; }
+
+        //WHY do we NOT need to fully implement a nullable numeric?
+        //Numerics have a default of zero.
+        //Numerics can only store a numeric (unless nullable)
+        //Numerica can be null if declared as nullable
+        //IF the numeric has additional criteria (e.g has to be greater than zero) THEN you can code the property as a Fully Implemented property
+        public int? NumberOfPanes { get; set; } //? bedeuted nullable
+
+
     }
 }
