51bff72bc3f8ec6a3e874963986e72be534e789d
cbitting1 <54864328+cbitting1@users.noreply.github.com>
2020-03-05 00:48:14 -0700

    Update

---
+++ BasicWebApp/WebApp/DDLData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp
+{
+    public class DDLData
+    {
+        public int valueField { get; set; }
+
+        public string displayField { get; set; }
+
+
+        //Constructor (wenn user nix eingibt dann wird dieser Constructor gerufen und macht nix heisst also der setzt valueField und displayField packt der nicht an)
+        public DDLData()
+        {
+
+        }
+
+
+
+        //Constructor wird gerufen wenn ein neues object created wird
+        public DDLData(int valuefield, string displayfield)
+        {
+            valueField = valuefield;
+            displayField = displayfield;
+        }
+    }
+}
--- BasicWebApp/WebApp/SamplePages/CommonControls.aspx.cs
+++ BasicWebApp/WebApp/SamplePages/CommonControls.aspx.cs
@@ -11,7 +11,7 @@ namespace WebApp.SamplePages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //Every time the page is processed (submitted to the web server) this method is the FIRST event that is processedthat you can easily see and interact with
+            //Every time the page is processed (submitted to the web server) this method is the FIRST event that is processed that you can easily see and interact with
 
             //This is a great place to do common code that is required on each process of the page 
             //Example: empty out old messages
@@ -21,7 +21,7 @@ namespace WebApp.SamplePages
             //Every control must have a unique name
             //The ID value is used to reference the control in your code behind
             //Since controls are instances of a class, all rules of OOP apply
-            MessageLabel.Text = "";
+            MessageLabel.Text = ""; //is the ID of the field that is shown on the bottom of the page (our output field)
 
 
             //The web page has a flag that can be checked to see if the web page is posting back
@@ -30,34 +30,48 @@ namespace WebApp.SamplePages
                 //If the page is NOT PostBack, it means that this is the first time the page has been displayed
                 //You can do page initialization by testing the IsPostBack
                 //Create a List<T> where T is a class that has 2 columns: a value and a text display
+                        //In die Klammer kommt die Type o fobjects (wo jetzt das T steht --> zb int oder float)
+                        //Das T ist der Type of object, in diesem Fall ist der Type of object eine class die wir programmmiert ist oder build in ist
 
+                //int x
+                //List DataCollection
+                List<DDLData> DataCollection = new List<DDLData>(); //DataCollection is the name of the property in the Class Page called "DDL Data
+                DataCollection.Add(new DDLData(1, "COMP1008")); // new DDLData --> ist ein Object createn
+                DataCollection.Add(new DDLData(3, "DMIT1508"));
+                DataCollection.Add(new DDLData(4, "DMIT2018"));
+                DataCollection.Add(new DDLData(2, "CPSC1517"));
 
-                //List<DDLData> DataCollection = new List<DDLData> < DDLData > ();
-                //DataCollection.Add(new DDLData(1, "COMP1008"));
-                //DataCollection.Add(new DDLData(3, "DMIT1508"));
-                //DataCollection.Add(new DDLData(4, "DMIT2018"));
-                //DataCollection.Add(new DDLData(2, "CPSC1517"));
-
-                
                 //Sorting a List<T>
                 //(x,y) are placeholders for 2 records at any given time in the sort
                 // => (lamda symbol) is part of the delegate syntax, read as "do the following"
                 //Comparing x to y is ascending
                 //Comparing y to x is descending
-
-                //DataCollection.Sort((x, y) => x.displayField.CompareTo(y.displayField));
-
-
-                //place the data into the dropdownlist control
-                    //a) assign the data collection to the "list" Control
-                
-                //CollectionList.DataSource = DataCollection;
+                DataCollection.Sort((x, y) => x.displayField.CompareTo(y.displayField));
 
 
+                //Place the data into the dropdownlist control
+                //a) assign the data collection to the "list" Control
+                CollectionList.DataSource = DataCollection;   //CollectionList is the ID of the DROPDOWN and DataCollection of the List we created
 
-                
+
+                //b) This step is required for specific "list" controls
+                //Indicate which data value is to be assigned to the Value field and the Display text field
+                //There are different styles in assigning this information
+                CollectionList.DataValueField = "valueField";
+                CollectionList.DataTextField = nameof(DDLData.displayField);  //don's preference
+                //Other controls that handle lists we don't need B; we only need A and C
+
+                //c)Bind your data to the actual control
+                CollectionList.DataBind(); //CollectionList is the name of the Dropdown
+
+                //d)OPTIONALLY you can place a prompt line on your control
+                CollectionList.Items.Insert(0, new ListItem("Select..."/*, "0"*/)); //Die erste 0 ist die Stelle des Indexes (in diesem Falle an "1." Stelle)
+                                //("Select...", "0") --> is a String and the Value (NOTE: we could also just put in the "Select..." without the Value)
             }
         }
+
+
+
 
         protected void SubmitButtonChoice_Click(object sender, EventArgs e)
         {
@@ -85,13 +99,13 @@ namespace WebApp.SamplePages
                 //This can be done using either .SelectedValue or .SelectedIndex
                 //.SelectedValue will match the control item value to the argument
                 //.SelectedIndex selects the control item based on it's physical index position
-                //The best policy is to use .SelectedValue 
+                //The best policy is to use .SelectedValue (IndexPosition and Value that we are trying to find do not neccessarely match up)
                 RadioButtonListChoice.SelectedValue = numberchoice.ToString();
 
                 //Set the checkbox
                 if(numberchoice == 2 || numberchoice == 4)
                 {
-                    CheckBoxChoice.Checked = true;
+                    CheckBoxChoice.Checked = true; //checked will turn on the checkbox (if we say "true" or off when we say "false")
                 }
                 else
                 {
@@ -104,9 +118,9 @@ namespace WebApp.SamplePages
                 //.SelectedIndex selects the control item based on it's physical index position
                 //The best policy is to use .SelectedValue 
                 CollectionList.SelectedValue = numberchoice.ToString();
+                //NOTE: Every ListControl that we have is gonna have the same type of behaviours (.SelectedValue/.SelectedIndex)
 
-
-                //Access data from your dropdownlist
+                //Accessing data from your dropdownlist
                 //This can be done using either .SelectedValue or .SelectedIndex or .SelectedItem
                 //.SelectedValue will return the value portion of your selected line of the control
                 //.SelectedIndex will return the selected line of the control physical index position
