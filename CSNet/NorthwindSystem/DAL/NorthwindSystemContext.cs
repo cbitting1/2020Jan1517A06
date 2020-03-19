d06919d4fed960bb1b0fc6c40c357f1876bb4ed4
cbitting1 <54864328+cbitting1@users.noreply.github.com>
2020-03-18 23:43:30 -0600

    Updated with comments

--- CSNet/NorthwindSystem/DAL/NorthwindSystemContext.cs
+++ CSNet/NorthwindSystem/DAL/NorthwindSystemContext.cs
@@ -13,14 +13,14 @@ using System.Data.Entity;
 
 namespace NorthwindSystem.DAL
 {
-    //Restrict Access to this classto ONLY other classes within this library: access priviledge of internal
+    //Restrict Access to this class to ONLY other classes within this library: access priviledge of internal
     //Secuity Measure
 
     //Connect to EntityFrameWork by inheriting DbContext
     internal class NorthwindSystemContext : DbContext
     {
         //You will need a constructor that passes the connection string name to EntityFrameWork via the inherited class DbContext
-        public NorthwindSystemContext() :base("NWDB")
+        public NorthwindSystemContext() :base("NWDB") //NorthwindSystemContext is the name of this class
         {
 
         }
@@ -28,6 +28,8 @@ namespace NorthwindSystem.DAL
         //Properties to interact with EntityFramework
         //These properties represent the whole table and all data of the SQL database
         public DbSet<Product> Products { get; set; } //one for each Entity
+        //<Product> is the name of the Entitiy in the Entities folder
+        //Products in plural because it's for all records not only for one record
 
     }
 }
