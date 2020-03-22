afd76314e6f331ae162a67843651b74f82397612
cbitting1 <54864328+cbitting1@users.noreply.github.com>
2020-03-22 17:11:21 -0600

    Lesson of 20 March

--- CSNet/NorthwindSystem/BLL/CategoryController.cs
+++ CSNet/NorthwindSystem/BLL/CategoryController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
 #region Additional Namespaces
 using NorthwindSystem.DAL;
 using NorthwindSystem.Entities;
+using System.ComponentModel; //used by ODS wizard exposure 
 #endregion
 
+
+//Note: everything that we expose here for the wizard is to fill the drop down list on the MultiRecordODS webpage
 namespace NorthwindSystem.BLL
 {
+    //expose the Class
+    [DataObject] 
     public class CategoryController
     {
+        [DataObjectMethod(DataObjectMethodType.Select,false)] //Exposure of the method
         public List<Category> Categories_List()
         {
             using (var context = new NorthwindSystemContext())
@@ -20,6 +26,8 @@ namespace NorthwindSystem.BLL
                 return context.Categories.ToList();
             }
         }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]//Exposure of the method
         public Category Categories_FindByID(int categoryid)
         {
             using (var context = new NorthwindSystemContext())
